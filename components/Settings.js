39d7a514f1e93ae4506532cbaa094134372af447
Jamie Holloway <jamie@cadiahq.com>
2021-11-13 12:08:22 +0000

    settings

--- components/Settings.js
+++ components/Settings.js
@@ -9,6 +9,21 @@ const Settings = ({swap, clear, reverse, copy, input, enc, dec}) => {
                 <textarea onInput={input} placeholder="Input key..."></textarea><br/>
                 <button onClick={enc}>Encrypt</button>
                 <button onClick={dec}>Decrypt</button>
+            </div>
+            <div>
+                hey
+                <ul>
+                  <li>Caesar</li>
+                  <li><a id="affine">Affine</a></li>
+                  <li><a id="substitutionA">Substitution (automatic)</a></li>
+                  <li><a id="substitutionM">Substitution (manual)</a></li>
+                  <li><a id="transpositionS">Transposition (simple)</a></li>
+                  <li><a id="transpositionC">Transposition (columnar)</a></li>
+                  <li><a id="vigenere">Vigenère</a></li>
+                  <li><a id="keyword">Keyword</a></li>
+                  <li><a id="polybius">Polybius Square</a></li>
+                  <li><a id="determine">Determine cipher</a></li>
+                </ul>
               </div>
         </div>
     )
